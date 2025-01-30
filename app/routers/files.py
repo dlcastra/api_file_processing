ff42dfc07f8d8ee804c5ad1df059b7bc8ef7432c
dlcastra <1dlcastra@gmail.com>
2025-01-30 16:36:16 +0100

    Added protection

--- app/routers/files.py
+++ app/routers/files.py
@@ -3,19 +3,20 @@ from fastapi.requests import Request
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.services.file_management import FileManagementService
+from app.utils import blacklist_check
 from settings.database import get_db
 from app.validators.file_validation import FileValidator, invalid_file
 
 router = APIRouter()
 
 
-@router.get("/history", status_code=200)
+@router.get("/history", dependencies=[Depends(blacklist_check)], status_code=200)
 async def files_history(request: Request, db: AsyncSession = Depends(get_db)):
     file_manager = FileManagementService(db)
     return await file_manager.get_files_history(request.session.get("user_id"))
 
 
-@router.post("/upload", status_code=201)
+@router.post("/upload", dependencies=[Depends(blacklist_check)], status_code=201)
 async def upload_file(request: Request, file: UploadFile = File(...), db: AsyncSession = Depends(get_db)):
     is_valid_file = FileValidator().validate_file(file)
     if not is_valid_file:
@@ -27,13 +28,13 @@ async def upload_file(request: Request, file: UploadFile = File(...), db: AsyncS
     return await file_manager.add_file(file, user_id)
 
 
-@router.get("/download/{file_id}", status_code=200)
+@router.get("/download/{file_id}", dependencies=[Depends(blacklist_check)], status_code=200)
 async def download_file(request: Request, file_id: int, db: AsyncSession = Depends(get_db)):
     file_manager = FileManagementService(db)
     return await file_manager.download_file(file_id, request.session.get("user_id"))
 
 
-@router.delete("/remove/{file_id}", status_code=204)
+@router.delete("/remove/{file_id}", dependencies=[Depends(blacklist_check)], status_code=204)
 async def remove_file(request: Request, file_id: int, db: AsyncSession = Depends(get_db)):
     file_manager = FileManagementService(db)
     return await file_manager.remove_file(file_id, request.session.get("user_id"))
