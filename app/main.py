2312dc2ded455d9743ade745428a9b37ed7ce009
dlcastra <1dlcastra@gmail.com>
2025-02-11 17:05:00 +0100

    CORS middleware

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from decouple import config
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
+from starlette.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
 
@@ -10,6 +11,7 @@ from app.routers import auth, files
 
 app = FastAPI()
 app.add_middleware(SessionMiddleware, secret_key=config("SECRET_KEY"), session_cookie="session_id", max_age=SESSION_AGE)
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 app.include_router(files.router, prefix="/files", tags=["FileProcessing"])
 
